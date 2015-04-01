781016669e50d89c5c953f5f9562bc8cdbc0e495
Ross Angle <rokitna@hotmail.com>
2015-03-31 21:31:38 -0700

    Added data for Steven Universe.

---
+++ data-for-steven-universe.js
@@ -0,0 +1,66 @@
+// data-for-steven-universe.js (part of Monday Comics)
+// Copyright 2015 Ross Angle. Released under the MIT License.
+//
+// The Steven Universe franchise and the characters therein are
+// trademarks of Cartoon Network. They are used here with the
+// intention to provide parody and literary analysis.
+
+"use strict";
+
+var dataForStevenUniverse = (function () {
+
+
+var characters = [];
+
+characters.push( { name: "Steven", qualities: [
+    "eating junk food",
+    "using Rose Quartz's powers",
+    "singing about life",
+    "watching TV",
+    "visiting Sadie and Lars"
+] } );
+
+characters.push( { name: "Garnet", qualities: [
+    "being in tune with herself",
+    "recognizing possible outcomes",
+    "getting hooked on video games",
+    "settling disputes",
+    "smashing things with giant gauntlets"
+] } );
+
+characters.push( { name: "Amethyst", qualities: [
+    "hoarding garbage",
+    "feeling like her existence is a mistake",
+    "grappling things with a whip",
+    "shapeshifting",
+    "having fun at the expense of other duties"
+] } );
+
+characters.push( { name: "Pearl", qualities: [
+    "cleaning up messes",
+    "training for battle",
+    "doing ballet",
+    "dreaming about the past",
+    "projecting holograms"
+] } );
+
+characters.push( { name: "Greg", qualities: [
+    "making music",
+    "operating a car wash",
+    "spending time with Steven",
+    "sifting through old belongings"
+] } );
+
+characters.push( { name: "Connie", qualities: [
+    "analyzing narratives",
+    "wearing glasses",
+    "reading books",
+    "getting mixed up in amazing gem powers"
+] } );
+
+return {
+    characters: characters
+};
+
+
+})();
